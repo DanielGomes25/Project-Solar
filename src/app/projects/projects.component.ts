3b66d9aa790f482849cddddead23ab3b45ab3661
Daniel <daniel.gomes.9@hotmail.com>
2024-06-07 18:01:09 -0300

    organizing components

---
+++ src/app/projects/projects.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-projects',
+  templateUrl: './projects.component.html',
+  styleUrl: './projects.component.scss',
+  standalone: true
+})
+export class ProjectsComponent {
+
+}
